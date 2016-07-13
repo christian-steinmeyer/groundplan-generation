30e2874b1f34ac4ff4d0b74410d83c7d97f7064e
Christian Steinmeyer <christian.steinmeyer@favendo.de>
2016-07-13 12:28:04 +0200

    bugfixing svg drawing

--- groundplangeneration/src/main/java/com/favendo/steinmeyer/svg/SVGUtils.java
+++ groundplangeneration/src/main/java/com/favendo/steinmeyer/svg/SVGUtils.java
@@ -2,11 +2,15 @@ package com.favendo.steinmeyer.svg;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.logging.Logger;
 
 /**
  * Created by Chrstian Steinmeyer on 04.07.2016.
  */
 public class SVGUtils {
+
+    private Logger log = Logger.getLogger(this.getClass().getName());
+
 
     final int STROKE_WIDTH = 2;
 
@@ -15,6 +19,8 @@ public class SVGUtils {
     String unit;
     float width;
     float height;
+    float xOffset;
+    float yOffset;
     Collection<Line> lines = new ArrayList<>();
 
     private SVGUtils() {
@@ -38,20 +44,19 @@ public class SVGUtils {
     }
 
     private void drawLine(Line line) {
-        float xOffset = height / 2;
-        float yOffset = width / 2;
         float x1 = line.x1 + xOffset;
         float x2 = line.x2 + xOffset;
-        float y1 = line.y1 + yOffset;
-        float y2 = line.y2 + yOffset;
+        // svg's origin is top left
+        float y1 = height - (line.y1 + yOffset);
+        float y2 = height - (line.y2 + yOffset);
+
         svg += "\t<line x1=\"" + x1 + unit + "\" y1=\"" + y1 + unit + "\" x2=\"" + x2 + unit +
                 "\" y2=\"" + y2 + unit + "\" stroke=\"rgb(0,0,0)\" stroke-width=\"" + STROKE_WIDTH +
                 "\" />\n";
     }
 
     public String build() {
-        determineWidth();
-        determineHeight();
+        determineSVGDimensions();
         svg = "<?xml version=\"1.0\" standalone=\"no\"?>\n" +
                 "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \n" +
                 "\t\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" +
@@ -66,24 +71,21 @@ public class SVGUtils {
         return svg;
     }
 
-    private void determineHeight() {
-        float max = Integer.MIN_VALUE;
-        float min = Integer.MAX_VALUE;
+    private void determineSVGDimensions() {
+        float maxY = Integer.MIN_VALUE;
+        float minY = Integer.MAX_VALUE;
+        float maxX = Integer.MIN_VALUE;
+        float minX = Integer.MAX_VALUE;
         for (Line line : lines) {
-            max = Math.max(max, Math.max(line.x1, line.x2));
-            min = Math.min(min, Math.max(line.x1, line.x2));
+            maxX = Math.max(maxX, Math.max(line.x1, line.x2));
+            minX = Math.min(minX, Math.min(line.x1, line.x2));
+            maxY = Math.max(maxY, Math.max(line.y1, line.y2));
+            minY = Math.min(minY, Math.min(line.y1, line.y2));
         }
-        height = Math.abs(max - min);
-    }
-
-    private void determineWidth() {
-        float max = Integer.MIN_VALUE;
-        float min = Integer.MAX_VALUE;
-        for (Line line : lines) {
-            max = Math.max(max, Math.max(line.y1, line.y2));
-            min = Math.min(min, Math.max(line.y1, line.y2));
-        }
-        width = Math.abs(max - min);
+        height = Math.abs(maxY - minY);
+        width = Math.abs(maxX - minX);
+        xOffset = -minX;
+        yOffset = -minY;
     }
 
 }
